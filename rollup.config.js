8e50540375c2b34c67d33593b625d239f4c48f53
无涯 <yutaozhou@YutaodeMacBook-Pro.local>
2021-12-10 18:25:56 +0800

    feat: template

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2021-12-10 17:35:43
  * @LastEditors: Fullsize
- * @LastEditTime: 2021-12-10 17:35:57
+ * @LastEditTime: 2021-12-10 18:24:01
  * @FilePath: /fun/rollup.config.js
  * @Author: Fullsize
  */
@@ -12,10 +12,9 @@ import babel from "rollup-plugin-babel"; // babel 插件
 import { eslint } from 'rollup-plugin-eslint'; // eslint插件
 
 export default {
-
-  input: './src/main.js', // 打包的入口文件
+  input: 'source/index.js', // 打包的入口文件
   output: {
-    name: 'my-pkg',  // 输入的包名
+    name: 'Fun',  // 输入的包名
     file: './bin/index.js', // 打包输出地址, 这里的导出地址就是package内main的地址
     format: 'umd' // 包类型
   },
---
+++ source/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Date: 2021-12-10 18:23:25
+ * @LastEditors: Fullsize
+ * @LastEditTime: 2021-12-10 18:23:26
+ * @FilePath: /fun/source/index.js
+ * @Author: Fullsize
+ */
+export { default as F } from './F.js';
