fcc6eca39e029abea7cf82d836500ef6e3e864cd
Yutao Zhou <yutaozhou@YutaodeMacBook-Pro.local>
2021-12-10 18:03:13 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+<!--
+ * @Date: 2021-12-10 17:27:56
+ * @LastEditors: Fullsize
+ * @LastEditTime: 2021-12-10 17:37:24
+ * @FilePath: /fun/README.md
+ * @Author: Fullsize
+-->
 # fun
+自用函数库(函数式编程)
---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+/*
+ * @Date: 2021-12-10 17:35:43
+ * @LastEditors: Fullsize
+ * @LastEditTime: 2021-12-10 17:35:57
+ * @FilePath: /fun/rollup.config.js
+ * @Author: Fullsize
+ */
+
+import resolve from 'rollup-plugin-node-resolve'; // 依赖引用插件
+import commonjs from 'rollup-plugin-commonjs'; // commonjs模块转换插件
+import babel from "rollup-plugin-babel"; // babel 插件
+import { eslint } from 'rollup-plugin-eslint'; // eslint插件
+
+export default {
+
+  input: './src/main.js', // 打包的入口文件
+  output: {
+    name: 'my-pkg',  // 输入的包名
+    file: './bin/index.js', // 打包输出地址, 这里的导出地址就是package内main的地址
+    format: 'umd' // 包类型
+  },
+  plugins: [  // 使用的插件
+    resolve(),
+    commonjs(),
+    babel({
+      exclude: 'node_modules/**',
+      runtimeHelpers: true,
+    }),
+    eslint({
+      throwOnError: true,
+      include: ['src/**'],
+      exclude: ['node_modules/**']
+    })
+  ],
+  ignore: [
+    "node_modules/**" // 忽略目录
+  ]
+
+}
