c0fd9ab4f44186da298808da45003db59b63ceff
无涯 <yutaozhou@YutaodeMacBook-Pro.local>
2021-12-14 14:09:35 +0800

    feat: core.js

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+/*
+ * @Date: 2021-12-14 11:08:28
+ * @LastEditors: Fullsize
+ * @LastEditTime: 2021-12-14 14:08:06
+ * @FilePath: /fun/index.d.ts
+ * @Author: Fullsize
+ */
+export = Fun;
+export as namespace Fun;
+
+declare const Fun: Fun.FunStatic;
+declare namespace Fun {
+    // tslint:disable-next-line no-empty-interface (This will be augmented)
+    interface FunStatic {}
+}
