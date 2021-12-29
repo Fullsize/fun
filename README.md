ad0421e6565862c88344cb8c10c84e85c36a3d94
无涯 <yutaozhou@YutaodeMacBook-Pro.local>
2021-12-29 16:44:16 +0800

    feat: update readme

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 <!--
  * @Date: 2021-12-10 17:27:56
  * @LastEditors: Fullsize
- * @LastEditTime: 2021-12-10 17:37:24
+ * @LastEditTime: 2021-12-29 16:43:32
  * @FilePath: /fun/README.md
  * @Author: Fullsize
 -->
 # fun
 自用函数库(函数式编程)
+
+下载
+``` shell
+yarn add fun-all
+npm install fun-all
+```
--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,18 @@
 /*
  * @Date: 2021-12-14 11:08:28
  * @LastEditors: Fullsize
- * @LastEditTime: 2021-12-14 14:08:06
+ * @LastEditTime: 2021-12-29 16:38:46
  * @FilePath: /fun/index.d.ts
  * @Author: Fullsize
  */
-export = Fun;
-export as namespace Fun;
-
-declare const Fun: Fun.FunStatic;
-declare namespace Fun {
-    // tslint:disable-next-line no-empty-interface (This will be augmented)
-    interface FunStatic {}
-}
+// export = Fun;
+// export as namespace Fun;
+// declare const Fun: Fun.FunStatic;
+// declare namespace Fun {
+//     // tslint:disable-next-line no-empty-interface (This will be augmented)
+//     interface FunStatic {}
+// }
+export function add(a: number, b: number): number;
+export function add(a: number): (b: number) => number;
+export function eq(a: any, b: any): boolean;
+export as namespace Fun
